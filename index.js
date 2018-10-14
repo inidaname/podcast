c070b9d9bb7c17e602442c435741fa244d8c36f3
Hassan Sani <saniyhassan@gmail.com>
2018-10-14 12:49:45 +0100

    Done for production on #hacktobarfest

--- index.js
+++ index.js
@@ -1,123 +1,132 @@
 "use strict";
-let broadcast;
-let frequency;
-const myRequest = new Request('./data/podcasts.json');
-let categories = new Promise((resolve, reject) => {
+var broadcast;
+var frequency;
+var myRequest = new Request('./data/podcasts.json');
+var categories = new Promise(function (resolve, reject) {
     fetch(myRequest)
         .then(function (response) {
         return response.json();
     })
         .then(function (response) {
-        let newCategory = [];
-        let finalCategory = [];
-        let lanCat = [];
-        let langFinal = [];
-        let dayCat = [];
-        let dayFinal = [];
-        let freqCat = [];
-        let freqFinal = [];
-        response.map((obj) => {
-            let cat = obj.category.split(',').map((v) => v.trim().toLowerCase());
-            let lang = obj.languages.split(',').map((v) => v.trim().toLowerCase());
-            let days = obj.broadcast.split(',').map((v) => v.trim().toLowerCase());
-            let freq = obj.updates.split(',').map((v) => v.trim().toLowerCase());
-            newCategory.push(...cat);
-            lanCat.push(...lang);
-            freqCat.push(...freq);
-            dayCat.push(...days);
+        var newCategory = [];
+        var finalCategory = [];
+        var lanCat = [];
+        var langFinal = [];
+        var dayCat = [];
+        var dayFinal = [];
+        var freqCat = [];
+        var freqFinal = [];
+        response.map(function (obj) {
+            var cat = obj.category.split(',').map(function (v) { return v.trim().toLowerCase(); });
+            var lang = obj.languages.split(',').map(function (v) { return v.trim().toLowerCase(); });
+            var days = obj.broadcast.split(',').map(function (v) { return v.trim().toLowerCase(); });
+            var freq = obj.updates.split(',').map(function (v) { return v.trim().toLowerCase(); });
+            newCategory.push.apply(newCategory, cat);
+            lanCat.push.apply(lanCat, lang);
+            freqCat.push.apply(freqCat, freq);
+            dayCat.push.apply(dayCat, days);
         });
-        newCategory.map((v) => {
+        newCategory.map(function (v) {
             if (v.length !== 0 && finalCategory.indexOf(v) < 0) {
                 finalCategory.push(v);
             }
         });
-        lanCat.map((v) => {
+        lanCat.map(function (v) {
             if (v.length !== 0 && langFinal.indexOf(v) < 0) {
                 langFinal.push(v);
             }
         });
-        dayCat.map((v) => {
+        dayCat.map(function (v) {
             if (v.length !== 0 && dayFinal.indexOf(v) < 0) {
                 dayFinal.push(v);
             }
         });
-        freqCat.map((v) => {
+        freqCat.map(function (v) {
             if (v.length !== 0 && freqFinal.indexOf(v) < 0) {
                 freqFinal.push(v);
             }
         });
-        let podCastList = [finalCategory, langFinal, dayFinal, freqFinal];
+        var podCastList = [finalCategory, langFinal, dayFinal, freqFinal];
         if (podCastList) {
-            return resolve({ podCastList, response });
+            return resolve({ podCastList: podCastList, response: response });
         }
         else {
             return reject('Empty Pod cast');
         }
     });
 });
-categories.then((res) => {
-    let re = res.podCastList;
-    let category = document.createElement('ul');
-    re[0].map((v, i) => {
-        let theChildren = document.createElement('li');
+categories.then(function (res) {
+    var re = res.podCastList;
+    var category = document.createElement('ul');
+    re[0].map(function (v, i) {
+        var theChildren = document.createElement('li');
         theChildren.appendChild(document.createTextNode(v.toUpperCase()));
         theChildren.setAttribute('class', 'subMenu');
-        theChildren.addEventListener('click', (event) => {
+        theChildren.addEventListener('click', function (event) {
             console.log(event);
         });
         return category.appendChild(theChildren);
     }).join(' ');
-    let cat = document.getElementById('category');
+    var cat = document.getElementById('category');
     cat.appendChild(category);
-    let languages = document.createElement('ul');
-    re[1].map((v, i) => {
-        let theChildren = document.createElement('li');
+    var languages = document.createElement('ul');
+    re[1].map(function (v, i) {
+        var theChildren = document.createElement('li');
         theChildren.appendChild(document.createTextNode(v.toUpperCase()));
         theChildren.setAttribute('class', 'subMenu');
-        theChildren.addEventListener('click', (event) => {
+        theChildren.addEventListener('click', function (event) {
             console.log(event);
         });
         return languages.appendChild(theChildren);
     }).join(' ');
-    let lang = document.getElementById('language');
+    var lang = document.getElementById('language');
     lang.appendChild(languages);
-    let releaseDays = document.createElement('ul');
-    re[3].map((v, i) => {
-        let theChildren = document.createElement('li');
+    var releaseDays = document.createElement('ul');
+    re[3].map(function (v, i) {
+        var theChildren = document.createElement('li');
         theChildren.appendChild(document.createTextNode(v.toUpperCase()));
         theChildren.setAttribute('class', 'subMenu');
-        theChildren.addEventListener('click', (event) => {
+        theChildren.addEventListener('click', function (event) {
             console.log(event);
         });
         return releaseDays.appendChild(theChildren);
     }).join(' ');
-    let day = document.getElementById('broadcast');
+    var day = document.getElementById('broadcast');
     day.appendChild(releaseDays);
-    let frquencies = document.createElement('ul');
-    re[2].map((v, i) => {
-        let theChildren = document.createElement('li');
+    var frquencies = document.createElement('ul');
+    re[2].map(function (v, i) {
+        var theChildren = document.createElement('li');
         theChildren.appendChild(document.createTextNode(v.toUpperCase()));
         theChildren.setAttribute('class', 'subMenu');
-        theChildren.addEventListener('click', (event) => {
-            let events = event.target;
+        theChildren.addEventListener('click', function (event) {
+            var events = event.target;
             console.log(re[2][i]);
         });
         return frquencies.appendChild(theChildren);
     }).join(' ');
-    let freq = document.getElementById('frequency');
+    var freq = document.getElementById('frequency');
     freq.appendChild(frquencies);
-    let contents = res.response;
+    var contents = res.response;
+    var podcastEl = document.createElement('ul');
+    podcastEl.classList.add('podcast');
+    contents.map(function (v, i) {
+        var mainURL = (v.url.substring(0, 5) !== 'http:') ? "http://" + v.url : v.url;
+        var listBe = "\n        <a href=\"" + mainURL + "\" target=\"_blank\">\n            <li class=\"podList\">\n                <div>\n                    <h3> " + v.name + " </h3>\n                    <img src=\"" + v.logo + "\" alt=\"" + v.name + "\">\n                    <ul>\n                        <li>\n                            <strong>Host Name:</strong> <br> " + v.host + "\n                        </li>\n                        <li>\n                            <strong>Description:</strong> <br> " + v.description + "\n                        </li>\n                        <li>\n                            <span><strong>Updates:</strong>" + v.updates + "</span>\n                             <span><strong>Release:</strong>\n                             " + v.broadcast + "</span>\n                        </li>\n                        <li>\n                            <strong>Category:</strong> <br> " + v.category + "\n                        </li>\n                        <li>\n                            <strong>Language:</strong> <br> " + v.languages + "\n                        </li>\n                    </ul>\n                </div>\n            </li>\n        </a>\n        ";
+        return podcastEl.append(document.createRange().createContextualFragment(listBe));
+    }).join(' ');
+    var mainList = document.getElementById('mainList');
+    mainList.appendChild(podcastEl);
     console.log(contents);
 })
-    .catch((reason) => {
+    .catch(function (reason) {
     console.log(reason);
 });
-const listMenu = document.querySelectorAll('#sideContent li');
-let nullMe;
-let listTrig;
-listMenu.forEach((el) => {
-    el.addEventListener('click', (ev) => {
-        listMenu.forEach((e) => {
+var listMenu = document.querySelectorAll('#sideContent li');
+var nullMe;
+var listTrig;
+listMenu.forEach(function (el) {
+    el.addEventListener('click', function (ev) {
+        listMenu.forEach(function (e) {
             if (e.getAttribute('id') !== ev.target.attributes.getNamedItem('id').value) {
                 e.children[0].style.opacity = '0';
                 e.children[0].style.height = '0px';
@@ -139,14 +148,20 @@ listMenu.forEach((el) => {
     });
 });
 function openNav() {
-    let sideBar = document.getElementById('sideBar');
-    sideBar.style.width = "300px";
-    let main = document.getElementById('main');
-    main.style.marginLeft = "300px";
-}
-function closeNav() {
-    let sideBar = document.getElementById('sideBar');
-    sideBar.style.width = "0px";
-    let main = document.getElementById('main');
-    main.style.marginLeft = "0px";
+    if (!document.body.classList.contains('menuMoved')) {
+        document.body.classList.add('menuMoved');
+        var sideBar = document.getElementById('sideBar');
+        sideBar.style.width = "300px";
+        var main = document.getElementById('main');
+        main.style.marginLeft = "300px";
+        main.style.width = "calc(100% - 300px)";
+    }
+    else {
+        var sideBar = document.getElementById('sideBar');
+        sideBar.style.width = "0px";
+        var main = document.getElementById('main');
+        main.style.marginLeft = "0px";
+        main.style.width = "100%";
+        document.body.classList.remove('menuMoved');
+    }
 }
